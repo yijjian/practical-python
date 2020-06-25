5f77dd59ab047bfa9f98dbe7889000802327c86a
yijjian <34033903+yijjian@users.noreply.github.com>
2020-06-25 14:56:43 +0800

    Update report.py
    have learned

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,26 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+def portfolio_cost(filename):
+    total_cost = 0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            nshares = int(row[1])
+            price = float(row[2])
+            total_cost += nshares * price
+    return total_cost
+
+def read_portfolio(filename):
+    portfolio = []
+    
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = {'name':row[0], 'shares':int(row[1]), 'price':float(row[2])}
+            portfolio.append(holding)
+    return portfolio
