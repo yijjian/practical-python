7156b204811ed23f8a58e475b3aa489f48b52396
yijjian <34033903+yijjian@users.noreply.github.com>
2020-06-25 20:47:25 +0800

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,33 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
+import csv
+
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
+    rows = csv.reader(f)
+    headers = next(rows)
+    print(headers)
+    total = 0
+    for num, row in enumerate(rows, start=1):
+        record = dict(zip(headers, row))
+        print(row)
+        try:
+            nshares = int(record['shares'])
+            price = float(record['price'])
+            total += nshares * price
+        except ValueError:
+            print(f'Row {num}: Bad row: {row}')
+    f.close
+    return total
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfoliodate.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
+
+
