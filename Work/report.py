c78751eb19f19d66b9c962aeb24099d67f57750a
yijjian <34033903+yijjian@users.noreply.github.com>
2020-06-25 18:17:25 +0800

    Update report.py

--- Work/report.py
+++ Work/report.py
@@ -24,3 +24,42 @@ def read_portfolio(filename):
             holding = {'name':row[0], 'shares':int(row[1]), 'price':float(row[2])}
             portfolio.append(holding)
     return portfolio
+
+def read_prices(filename):
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except Exception:
+                print('It\'s over')
+    return prices
+
+def make_report(portfolio, prices):
+    report = []
+
+    for stk in portfolio:
+        report.append((stk['name'],stk['shares'],prices[stk['name']],prices[stk['name']] - stk['price']))
+    return report
+
+    
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+report = make_report(portfolio, prices)
+
+print('{:>16} {:>16} {:>16} {:>16}'.format('Name','Shares','Price','Change'))
+value = ' -'
+print(f'{value * 8} {value * 8} {value * 8} {value * 8}')
+for name, shares, price, change in report:
+    price = '$' + str('%0.2f'% price)
+    print(f'{name:>16s} {shares:>16d} {price:>16} {change:>16.2f}')
+
+'''
+print('{:>16} {:>16} {:>16} {:>16}'.format('Name','Shares','Price','Change'))
+value = ' -'
+print(f'{value * 8} {value * 8} {value * 8} {value * 8}')
+for stk in portfolio:
+    print('{name:>16} {shares:>16} {price:>16.2f} {change:>16.2f}'.format_map(stk))
+'''
