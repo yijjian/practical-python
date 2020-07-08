19bdf99eb0513ddc842b1d0a4b8f76d7a02d7e40
yijjian <34033903+yijjian@users.noreply.github.com>
2020-07-08 22:08:39 +0800

    Update fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,21 +4,45 @@
 
 import csv
 
-def parse_csv(filename):
 
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimiter)
 
-        headers = next(rows)
         records = []
-        for row in rows:
             if not row:
                 continue
             record = dict(zip(headers, row))
-            records.append(record)
 
     return records
 
-portfolio = parse_csv('Data/portfolio.csv')
 
-print(portfolio)
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = None
+
+
+            
+            for row in rows:
+                if not row:
+                    continue
+                if indices:
+                    row = [row[index] for index in indices]
+                if types:
+                    row = [func(val) for func, val in zip(types, row)]
+                record = dict(zip(headers, row))
+                records.append(record)
+
+            
+        
+        else:
+            for row in rows:
+                if not row:
+                    continue
+                if types:
+                    record = tuple([func(val) for func, val in zip(types, row)])
+                    records.append(record)
+        return records
+
+
