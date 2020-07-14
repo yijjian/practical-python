d8db0799205ad28b81045f9136fc72399ff17bfe
yijjian <34033903+yijjian@users.noreply.github.com>
2020-07-14 21:54:30 +0800

    Update fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -2,38 +2,22 @@
 #
 # Exercise 3.3
 
-<<<<<<< HEAD
-=======
 import csv
 
 def parse_csv(filename):
-
+    '''
+    Parse a CSV file into a list of records
+    '''
     with open(filename) as f:
         rows = csv.reader(f)
 
+        # Read the file headers
         headers = next(rows)
         records = []
         for row in rows:
-            if not row:
+            if not row:    # Skip rows with no data
                 continue
             record = dict(zip(headers, row))
             records.append(record)
 
     return records
-
-portfolio = parse_csv('Data/portfolio.csv')
-
-print(portfolio)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 19bdf99... Update fileparse.py
-=======
->>>>>>> parent of 19bdf99... Update fileparse.py
-=======
->>>>>>> parent of 19bdf99... Update fileparse.py
-=======
->>>>>>> parent of 19bdf99... Update fileparse.py
-=======
->>>>>>> parent of 19bdf99... Update fileparse.py
