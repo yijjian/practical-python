854d9695b9b96c1ac1edc22c7e911d308c27376b
yijjian <34033903+yijjian@users.noreply.github.com>
2020-07-14 22:40:21 +0800

    Update fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,20 +4,48 @@
 
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=None):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
-        rows = csv.reader(f)
+        if delimiter:
+            rows = csv.reader(f, delimiter=delimiter)
+        else:
+            rows = csv.reader(f)
 
-        # Read the file headers
-        headers = next(rows)
-        records = []
-        for row in rows:
-            if not row:    # Skip rows with no data
-                continue
-            record = dict(zip(headers, row))
-            records.append(record)
+        if has_headers == True:
+
+            # Read the file headers
+            headers = next(rows)
+
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
+
+            
+            records = []
+            for row in rows:
+                if types:
+                    row = [func(val) for func, val in zip(types, row)]
+                if not row:    # Skip rows with no data
+                    continue
+                if indices:
+                    row = [row[index] for index in indices]
+                    
+                record = dict(zip(headers, row))
+                records.append(record)
+
+        else:
+            records = []
+            for row in rows:
+                if types:
+                    row = tuple([func(val) for func, val in zip(types, row)])
+                if not row:    # Skip rows with no data
+                    continue
+                records.append(row)
+            
 
     return records
