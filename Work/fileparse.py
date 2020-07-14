87f8928cdf3b1bfcdbf8ff3be72596b6268d26d4
yijjian <34033903+yijjian@users.noreply.github.com>
2020-07-14 21:36:13 +0800

    Update fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -2,3 +2,26 @@
 #
 # Exercise 3.3
 
+<<<<<<< HEAD
+=======
+import csv
+
+def parse_csv(filename):
+
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row:
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
+
+portfolio = parse_csv('Data/portfolio.csv')
+
+print(portfolio)
+>>>>>>> parent of 19bdf99... Update fileparse.py
