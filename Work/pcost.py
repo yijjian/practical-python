89abc4fda0e43ddc701c830065dc78086d4b6254
yijjian <34033903+yijjian@users.noreply.github.com>
2020-07-17 17:04:02 +0800

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -7,7 +7,7 @@ def portfolio_cost(filename):
     Computes the total cost (shares*price) of a portfolio file
     '''
     portfolio = report.read_portfolio(filename)
-    return sum([s['shares'] * s['price'] for s in portfolio])
+    return sum([s.shares * s.price for s in portfolio])
 
 def main(args):
     if len(args) != 2:
