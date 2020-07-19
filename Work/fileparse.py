6bc469fdaa9f7214c929ee563a75f3a92d796700
yijjian <34033903+yijjian@users.noreply.github.com>
2020-07-19 21:20:37 +0800

    Update fileparse.py

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,61 +1,47 @@
 # fileparse.py
-#
-# Exercise 3.3
-
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=None):
+def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
     '''
-    Parse a CSV file into a list of records
+    Parse a CSV file into a list of records with type conversion.
     '''
-    with open(filename) as f:
-        if delimiter:
-            rows = csv.reader(f, delimiter=delimiter)
+    if select and not has_headers:
+        raise RuntimeError('select requires column headers')
+
+    rows = csv.reader(lines, delimiter=delimiter)
+
+    # Read the file headers (if any)
+    headers = next(rows) if has_headers else []
+
+    # If specific columns have been selected, make indices for filtering and set output columns
+    if select:
+        indices = [ headers.index(colname) for colname in select ]
+        headers = select
+
+    records = []
+    for rowno, row in enumerate(rows, 1):
+        if not row:     # Skip rows with no data
+            continue
+
+        # If specific column indices are selected, pick them out
+        if select:
+            row = [ row[index] for index in indices]
+
+        # Apply type conversion to the row
+        if types:
+            try:
+                row = [func(val) for func, val in zip(types, row)]
+            except ValueError as e:
+                if not silence_errors:
+                    print(f"Row {rowno}: Couldn't convert {row}")
+                    print(f"Row {rowno}: Reason {e}")
+                continue
+
+        # Make a dictionary or a tuple
+        if headers:
+            record = dict(zip(headers, row))
         else:
-            rows = csv.reader(f)
-
-        if has_headers == True:
-
-            # Read the file headers
-            headers = next(rows)
-
-            if select:
-                indices = [headers.index(colname) for colname in select]
-                headers = indices
-            else:
-                indices = []
-
-            
-            records = []
-            for row in rows:
-                if types:
-                    try:
-                        row = [func(val) for func, val in zip(types, row)]
-                    except ValueError as e:
-                        pass
-                if not row:    # Skip rows with no data
-                    continue
-                if indices:
-                    row = [row[index] for index in indices]
-                    print(row)
-                    
-                record = dict(zip(headers, row))
-                records.append(record)
-
-        else:
-            if select and has_headers == False:
-                raise RuntimeError('select argument requires column headers')
-            records = {}
-            for i, row in enumerate(rows, start=1):
-                if types:
-                    try:
-                        row = tuple([func(val) for func, val in zip(types, row)])
-                        name, price = row
-                    except ValueError as e:
-                        pass
-                if not row:    # Skip rows with no data
-                    continue
-                records[name] = price
-            
+            record = tuple(row)
+        records.append(record)
 
     return records
