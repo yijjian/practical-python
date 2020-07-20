c077d20ffb6eebd7b89279a8a3e90ec172b23e0e
yijjian <34033903+yijjian@users.noreply.github.com>
2020-07-20 22:58:01 +0800

    Update stock.py

--- Work/stock.py
+++ Work/stock.py
@@ -1,8 +1,11 @@
-class Stock:
+class Stock:    
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+
+    def __repr__(self):
+        return f'Stock(\'{self.name}\', {self.shares}, {self.price})'
 
     def cost(self):
         cost = self.shares * self.price
